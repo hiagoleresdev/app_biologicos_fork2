a2eb172d458c901c6c7fd8b50508e67c48b99147
hiago <hiagoleresdev@gmail.com>
2022-11-04 13:40:34 -0300

    adicionando server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname = './dist/app-biologicos'))
+
+app.get('/*', (req,res) =>{
+    res.sendFile(__dirname + './dist/app-biologicos/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log("Servidor na: "+PORT);
+})
+
+//app_biologicos_v1\dist\app-biologicos\index.html
